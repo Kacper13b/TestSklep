5d8887c801a0d2586f4b327df523ba7d3e0228a7
Kacper13b <kacper.banasik13b@gmail.com>
2021-02-03 17:30:23 +0100

    added order model

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -25,6 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart1')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
