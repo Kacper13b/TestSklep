2344ab62eb20d5282ba8ef6f886c5dbc8f70e23f
Kacper13b <kacper.banasik13b@gmail.com>
2021-02-11 15:28:58 +0100

    added order email with Celery, RabbitMQ

---
+++ myshop/orders/tasks.py
@@ -0,0 +1,11 @@
+from celery import task
+from django.core.mail import send_mail
+from .models import Order
+
+@task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = 'Zamówienie nr {}'.format(order.id)
+    message = 'Witaj, {}!\nDziękujemy za zakup w naszym sklepie!\nIdentyfikator zamówienia to {}'.format(order.first_name, order.id)
+    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    return mail_sent
