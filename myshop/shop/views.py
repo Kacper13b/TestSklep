93d64fe2d7089e8fadb47b3ccc573ff8cadab53a
Kacper13b <kacper.banasik13b@gmail.com>
2021-04-14 19:03:25 +0200

    ddd

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -17,7 +17,8 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    produt_image = ProductImage.objects.filter(product=product)
+    product_image = ProductImage.objects.filter(product=product)
     return render(request, 'product/detail.html', {'product': product,
                                                    'cart_product_form': cart_product_form,
-                                                   'product_image': produt_image})
+                                                   'product_image': product_image})
+
